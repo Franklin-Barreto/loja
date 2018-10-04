c3d3ec9b99c48f7b7963fa42c12b11fbe38ba6cc
Franklin-Barreto <franklin-barreto@hotmail.com>
2018-10-04 12:06:33 -0300

    usando funcao extract

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,27 +1,24 @@
-<?php include("cabecalho.php");
-      include("conecta.php");
-      include("banco-produto.php");
-      include("logica-usuario.php");
+<?php
+
+include ("cabecalho.php");
+include ("conecta.php");
+include ("banco-produto.php");
+include ("logica-usuario.php");
 
 verificaUsuario();
 
-$nome = $_POST["nome"];
-$preco = $_POST["preco"];
-$descricao = $_POST["descricao"];
-$categoria_id = $_POST['categoria_id'];
+extract($_POST, EXTR_OVERWRITE);
+$usado = isset($usado) ? "true" : "false";
 
-if(array_key_exists('usado', $_POST)) {
-    $usado = "true";
+if (insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado)) {
+    ?>
+<p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php
+
 } else {
-    $usado = "false";
-}
-
-if(insereProduto($conexao, $nome, $preco, $descricao, $categoria_id, $usado)) { ?>
-    <p class="text-success">O produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
-<?php } else {
     $msg = mysqli_error($conexao);
-?>
-    <p class="text-danger">O produto <?= $nome; ?> não foi adicionado: <?= $msg ?></p>
+    ?>
+<p class="text-danger">O produto <?= $nome; ?> não foi adicionado: <?= $msg ?></p>
 <?php
 }
 ?>
--- banco-usuario.php
+++ banco-usuario.php
@@ -2,6 +2,8 @@
 function buscaUsuario($conexao, $email, $senha) {
     $senhaMd5 = md5($senha);
     $query = "select * from usuarios where email='{$email}' and senha='{$senhaMd5}'";
+    //var_dump($query);
+    //die();
     $resultado = mysqli_query($conexao, $query);
     $usuario = mysqli_fetch_assoc($resultado);
     return $usuario;
